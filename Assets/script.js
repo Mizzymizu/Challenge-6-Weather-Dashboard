3b4ed4144051d897049737e8b77d2400237a3903
Dan Levi Obello <danleviobello@Dans-MacBook-Pro.local>
2023-07-13 09:14:32 -0500

    Added dayJS link just in case I need it. Added variables I think I need to so at manipulate the DOM

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,5 @@
+// Create variables for API key and city selector
 var apiKey = "14b6b5eb484bfeb72ae69cd2216e5328";
-var city = ""
+var citySelector = document.querySelector(".search-input");
+// Created variable for the queryURL to make the API call
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
