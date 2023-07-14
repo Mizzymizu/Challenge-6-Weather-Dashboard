5cedc5573c3bea1cb45bd9b99ee81a9429e530b2
Dan Levi Obello <danleviobello@Dans-MacBook-Pro.local>
2023-07-13 22:06:27 -0500

    eventListener function to set data to local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,22 @@
 // Create variables for API key and city selector
 var apiKey = "14b6b5eb484bfeb72ae69cd2216e5328";
-var citySelector = document.querySelector(".search-input");
+var citySelector = document.getElementById("search-input");
 // Created variable for the queryURL to make the API call
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + selectedCity + "&appid=" + apiKey;
+var selectedCity = localStorage.getItem("city");
+
+// Add event listener to the search field
+citySelector.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var city = citySelector.value.trim();
+    localStorage.setItem("city", city);
+});
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+    })
+  .catch(() => {
+    msg.textContent = "Please search for a valid city";
+    });
